a9b2c01ca6c94e345fe510eefa2e9e9400e59b61
Drake Rentrop <drakerentrop@gmail.com>
2018-11-14 21:50:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  return false if integer < 2
+  (2..integer - 1).each do |x|
+    if (integer % x) == 0
+      return false
+    else
+      true
+    end
+  end
+  
